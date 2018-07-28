ff256be12ef0ff54ae0dead66bf589d4b71fa8d9
r0bulous <rrwinn@gmail.com>
2018-07-28 18:59:28 -0400

    changed shared styling elements, converted belts index from HTML table to bootstrap and began layout of boots grid

---
+++ WardrobeMVC/Models/Belt.cs
@@ -0,0 +1,9 @@
+﻿namespace WardrobeMVC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+
+}
---
+++ WardrobeMVC/Models/PartialClasses.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WardrobeMVC.Models
+{
+    //[MetadataType(typeof(BeltMetadata))]
+    //public partial class Belt
+    }
