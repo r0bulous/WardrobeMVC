5ba1af152e3a33453b6d935937ae931c0166d5e9
r0bulous <rrwinn@gmail.com>
2018-07-29 17:01:51 -0400

    added partial class to update displaynames for shirts

---
+++ WardrobeMVC/Models/PantViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WardrobeMVC
+{
+    [MetadataType(typeof(Pantmetadata))]
+    public partial class Pant
+
+    {
+    }
+
+    //Display names for belt columns
+
+    public class Pantmetadata
+    {
+        [DisplayName("Pants")]
+        public string PantName { get; set; }
+
+        [DisplayName("Photo")]
+        public string PantPhoto { get; set; }
+
+        [DisplayName("Color")]
+        public string PantColor { get; set; }
+
+        [DisplayName("Type")]
+        public string PantType { get; set; }
+
+        [DisplayName("Season")]
+        public string PantSeason { get; set; }
+
+        [DisplayName("Occasion")]
+        public string PantOccasion { get; set; }
+
+        [DisplayName("Description")]
+        public string PantNotes { get; set; }
+
+    }
+}
