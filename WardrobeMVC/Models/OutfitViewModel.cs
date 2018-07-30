17f30aa46beb535a88b8a3410cc7b244fb902ad3
r0bulous <rrwinn@gmail.com>
2018-07-29 20:39:06 -0400

    converted shirt details view from HTML table to CSS using layout in shared stylesheet

---
+++ WardrobeMVC/Models/OutfitViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WardrobeMVC
+{
+    [MetadataType(typeof(Outfitmetadata))]
+    public partial class Outfit
+
+    {
+    }
+
+    //Display names for outfit columns
+
+    public class Outfitmetadata
+    {
+        [DisplayName("Outfit")]
+        public string OutfitName { get; set; }
+
+        [DisplayName("Description")]
+        public string OutfitNotes { get; set; }
+
+    }
+}
